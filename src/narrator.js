edd9910ed7a15dca33156a9b93327ab25c2f6827
Grant Ammons <gammons@gmail.com>
2019-05-10 10:16:37 -0400

    wip

--- src/narrator.js
+++ src/narrator.js
@@ -1,9 +1,34 @@
-exports.handler = function(event, context, callback) {
-  const qa = require("qs");
-  const parsed = qa.parse(event.body);
+const qa = require("qs");
+const http = require("https");
 
-  callback(null, {
-    statusCode: 200,
-    body: "_" + parsed.text + "_"
-  });
+exports.handler = function(event, context, callback) {
+  const parsed = qa.parse(event.body);
+  console.log("parsed is ", parsed);
+  const text = parsed.text;
+  const token = process.env.BOT_TOKEN;
+
+  // request.post("https://slack.com/api/chat.postMessage", {
+  //   json: {
+  //
+  //   }
+  // });
+
+  // const opts = {
+  //   hostname: "slack.com",
+  //   port: 443,
+  //   path: "/api/chat.postMessage",
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // }
+
+  // const req = http.request(options, (res) =>  {
+  //   console.log("res is ", res);
+  //   callback(null, { statusCode: 200 });
+  // });
+  //
+  // req.on('error', e => {
+  //   console.log(e);
+  // });
 };

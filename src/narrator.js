5001cff39075af883891ff927f6629538b5c888e
Grant Ammons <gammons@gmail.com>
2019-05-10 09:42:10 -0400

    maybe this

--- src/narrator.js
+++ src/narrator.js
@@ -1,11 +1,9 @@
 exports.handler = function(event, context, callback) {
   const qa = require("qs");
   const parsed = qa.parse(event.body);
-  console.log("parsed:", parsed);
-  console.log("parsed.text = ", parsed.text);
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ text: parsed.text })
+    body: parsed.text
   });
 };

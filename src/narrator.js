dc40d3b05321fbc30846156bb5981a9e137bb216
Grant Ammons <gammons@gmail.com>
2019-05-10 09:40:51 -0400

    wip

--- src/narrator.js
+++ src/narrator.js
@@ -1,11 +1,11 @@
 exports.handler = function(event, context, callback) {
-  console.log("event is ", event);
-  console.log(event.queryStringParameters);
-
-  const resp = { text: event.queryStringParameters.text };
+  const qa = require("qs");
+  const parsed = qa.parse(event.body);
+  console.log("parsed:", parsed);
+  console.log("parsed.text = ", parsed.text);
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(resp)
+    body: JSON.stringify({ text: parsed.text })
   });
 };

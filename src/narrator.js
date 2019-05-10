8d6cbbc99f35b611ee700fc42604096d8aa7c4be
Grant Ammons <gammons@gmail.com>
2019-05-10 10:36:28 -0400

    wip

--- src/narrator.js
+++ src/narrator.js
@@ -6,29 +6,39 @@ exports.handler = function(event, context, callback) {
   console.log("parsed is ", parsed);
   const text = parsed.text;
   const token = process.env.BOT_TOKEN;
+  console.log(`token = '${token}'`);
 
-  // request.post("https://slack.com/api/chat.postMessage", {
-  //   json: {
-  //
-  //   }
-  // });
+  const data = JSON.stringify({
+    channel: parsed.channel_id,
+    text: `_${text}_`
+  });
 
-  // const opts = {
-  //   hostname: "slack.com",
-  //   port: 443,
-  //   path: "/api/chat.postMessage",
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   }
-  // }
+  const opts = {
+    hostname: "slack.com",
+    port: 443,
+    path: "/api/chat.postMessage",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+      Authorization: `Bearer ${token}`
+    }
+  };
 
-  // const req = http.request(options, (res) =>  {
-  //   console.log("res is ", res);
-  //   callback(null, { statusCode: 200 });
-  // });
-  //
-  // req.on('error', e => {
-  //   console.log(e);
-  // });
+  const req = http.request(opts, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", d => {
+      process.stdout.write(d);
+    });
+
+    callback(null, { body: "", statusCode: 200 });
+  });
+
+  req.on("error", e => {
+    console.log("error is ", e);
+  });
+
+  req.write(data);
+  req.end();
 };
